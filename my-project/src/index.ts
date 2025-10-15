82fd990b3b347e00c853c3c91e4ba0c941444a09
Marcus <marcussousa.pereira@gmail.com>
2025-10-14 22:05:46 -0300

    Atualização

--- my-project/src/index.ts
+++ my-project/src/index.ts
@@ -1,10 +1,24 @@
+// Entry point: Sets up an Express server to serve the index page
+// Import Express (web server framework)
+// Import Express and path using CommonJS require for compatibility
 const express = require('express');
+const path = require('path');
+// __dirname is available by default in CommonJS
 
+// Create an Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware and routes can be set up here
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+// Serve the index.html file when the user visits the root URL
+app.get('/', (req: any, res: any) => {
+	// __dirname is the directory of the current file
+	// We use path.join to find the index.html file in the view folder
+	res.sendFile(path.join(__dirname, 'view', 'index.html'));
 });
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+	console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+// You can still use your MVC classes here if you want to add more features
