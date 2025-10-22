eee28f147a97334400adf06ab1545c701b6b1c2e
Marcus <marcussousa.pereira@gmail.com>
2025-10-22 18:50:56 -0300

    chore: remove duplicação de pasta my-project/my-project

--- my-project/my-project/src/view/UserView.ts
+++
@@ -1,8 +0,0 @@
-// View: Handles how data is presented to the user
-// This is a simple view for displaying user information
-export class UserView {
-  // Displays a message to the user (console for simplicity)
-  showMessage(message: string): void {
-    console.log(message);
-  }
-}
