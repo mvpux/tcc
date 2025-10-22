8ec4e0e2a4e8ca795371e0996efb4ac7df4fb8fd
Marcus <marcussousa.pereira@gmail.com>
2025-10-22 19:03:49 -0300

    chore: remove duplicações e sincroniza estrutura com versão local

--- my-project/src/styles/theme.ts
+++
@@ -1,8 +0,0 @@
-// import * as tokens from '../../build/js/tokens';
-
-// export const theme = {
-//   colors: tokens["tailwind colors"],
-//   spacing: tokens["tailwind primitives"],
-//   radius: tokens["tailwind primitives"]
-//   // Adicione outros grupos (tipografia, shadows, etc)
-// };
